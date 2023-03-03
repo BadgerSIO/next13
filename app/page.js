084ec3deb38a585a91aad63f880e2d12d56fc7df
Saad Ibne Omar <86186495+BadgerSIO@users.noreply.github.com>
2023-03-03 11:56:46 +0600

    do i have to build every time

--- app/page.js
+++ app/page.js
@@ -9,6 +9,11 @@ export default async function Home() {
       <h1 className={`font-playfair text-4xl lg:text-7xl text-center`}>
         Next <span className={styles.coloredTxt}>13</span>
       </h1>
+      <p className="text-sm max-w-2xl my-5 mx-auto text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+        consequatur, possimus voluptate optio minus animi aliquid adipisci
+        incidunt dolorem eveniet,
+      </p>
       <div className="grid gap-6 grid-cols-fluid mt-5">
         {data.map((game) => (
           <GamesCard key={game._id} game={game} />
